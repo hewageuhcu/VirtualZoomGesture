c78955d47d0e7807f7a9ace943d3a9a94f81e3a3
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-05 02:10:37 +0900

    created a detector

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import cv2
+from cvzone.HandDTrackingModule import HandDetector
 cap=cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
 
+
+detector=HandDetector(detectionCon=0.8)
+
 while True:
     success, img=cap.read()
+    hands,img=detector.findHands(img)
     cv2.imshow("Image",img)
     cv2.waitKey(1)
